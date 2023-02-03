3e0cab4225bb98b2af27d7519e17082ea900f035
Martijn <mhjnieuwenburg@gmail.com>
2023-02-03 14:42:19 +0100

    update 0.3
    * Flip cards
    * Password hidde
    * Fontsadded
    * Overlay menu z index 101

--- Js/Login/cardflip.js
+++ Js/Login/cardflip.js
@@ -8,3 +8,21 @@ loginButton.onclick = function(){
 registerButton.onclick = function(){
 	document.querySelector("#flipper").classList.toggle("flip");
 }
+
+
+	//Login password show
+
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#id_password');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+
+
+
