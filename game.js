c799180a30c995ac1a45918518bd5b2a5cf8756b
ItsMeMichaeluh <113515104+ItsMeMichaeluh@users.noreply.github.com>
2023-02-07 13:52:50 +0100

    michiel
    beginning of the ending screen

--- game.js
+++ game.js
@@ -1,16 +1,17 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
 let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
+var display = 0;
+var div = document.getElementById('game-over')
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
+    display = 1
+    endScreen()  
     return
   }
 
@@ -43,3 +44,22 @@ function draw() {
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
+
+function endScreen() {
+  if(display == 1) {
+    div.style.display = 'block';
+    display = 0;
+  }
+  else
+  {
+    div.style.display = 'none';
+    display = 1
+}
+}
+
+function game_restart() {
+  document.getElementById(btn); {
+    window.location = '/'
+  }
+}
+
