187d6deffac78ecbf98d2f9b59de005cc75348c1
Jeroen038 <97095929@st.deltion.nl>
2023-02-20 13:05:00 +0100

    reaction game
    beta versie af

--- Games/reactiongame/reaction.js
+++ Games/reactiongame/reaction.js
@@ -1,6 +1,44 @@
-randomTime = 0;
-function setRandom() {
-    randomTime = Math.random() * 101 | 0;
+let green = false;
+let startTime;
+
+function startGame() {
+    document.getElementById("startGame").style.visibility = "hidden";
+    document.getElementById("field").style.background = "red";
 }
-setInterval(setRandom, 5000);
-console.log(randomTime)
+
+const startButton = document.querySelector("#startGame");
+
+startButton.addEventListener("click", () => {
+    const delay = Math.floor(Math.random() * 6) + 2;
+    setTimeout(() => {
+        console.log(`Timer done after ${delay} seconds!`);
+        document.getElementById("field").style.background = "#00ff00";
+        document.getElementById("field").innerHTML = "NOW!!!";
+        document.getElementById("field").style.fontSize = "500%";
+        green = true;
+        startTime = Date.now();
+    }, delay * 1000);
+});
+
+function field() {
+    if (green) {
+        const elapsedTime = Date.now() - startTime;
+        const restartButton = document.createElement("button");
+        restartButton.innerHTML = "Restart";
+        restartButton.classList.add("restart-button");
+        document.getElementById("field").style.fontSize = "100%";
+        document.getElementById("field").innerHTML = `Je klikte in ${elapsedTime} millisecondes!<br><br>`;
+        document.getElementById("field").appendChild(restartButton);
+        restartButton.addEventListener("click", restart);
+    }
+}
+
+
+
+function restart() {
+    location.reload();
+}
+
+
+document.getElementById("field").addEventListener("click", field);
+
