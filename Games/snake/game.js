009214cbf25231ff77fb1be8071b549a4172815f
Jeroen038 <97095929@st.deltion.nl>
2023-03-15 15:31:50 +0100

    help

--- Games/snake/game.js
+++ Games/snake/game.js
@@ -1,4 +1,4 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection, setSnakeSpeed} from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
 
@@ -7,6 +7,7 @@ let gameOver = false
 const gameBoard = document.getElementById('game-board')
 var display = 0;
 var game_Over = document.getElementById('game-over')
+var start_game = document.getElementById('start-screen')
 
 function main(currentTime) {
   if (gameOver) {
@@ -14,18 +15,6 @@ function main(currentTime) {
     endScreen()  
     return
   }
-
-  const slow1 = document.getElementById("slow");
-  slow1.addEventListener("click", setSnakeSpeed, 5);
-
-  const normal1 = document.getElementById("normal");
-  normal1.addEventListener("click", setSnakeSpeed, 7);
-
-  const fast1 = document.getElementById("fast");
-  fast1.addEventListener("click", setSnakeSpeed, 10);
-
-  const expert1 = document.getElementById("expert");
-  expert1.addEventListener("click", setSnakeSpeed, 15);
 
 
   window.requestAnimationFrame(main)
