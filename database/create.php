461ce1f489c1b656416fa21479b869088ee8733f
Jeroen038 <97095929@st.deltion.nl>
2023-03-22 15:30:22 +0100

    data

---
+++ database/create.php
@@ -0,0 +1,33 @@
+<?php
+include "dbconfig.php";
+
+if(isset($_POST['submit'])){
+    $username = $_POST ['username'];
+    $voornaam = $_POST ['fullname'];
+    $email = $_POST ['email'];
+    $wachtwoord = $_POST ['userpassword'];
+
+    //wachtwoord encrypten
+
+$hash = password_hash($password, PASSWORD_BCRYPT);
+
+$query = "INSERT INTO users (username, fullname, email, userpassword) values (:username, :fullname, :email, :userpassword)";
+$insert = $database->prepare($query);
+$insert->bindParam(':gebruikersnaam', $username);
+$insert->bindParam(':voornaam', $fullname);
+$insert->bindParam(':email', $email);
+$insert->bindParam(':wachtwoord', $hash);
+$insert->execute();
+
+header('Location: login.html');
+}
+
+$id = $_GET;["id"];
+$query = "SELECT * FROM users WHERE id = ?";
+
+$users = $database->prepare($query);
+$data = array($id);
+$users->execute($data);
+$row = $users->fetch();
+
+?>
---
+++ database/update.php
@@ -0,0 +1,24 @@
+<?php
+include "dbconfig.php";
+if(isset(S_POST['submit'])){
+    $id = $_POST["id"];
+    //$id = $_POST["userId"];
+    $username = $_POST["username"];
+    $voornaam = $_POST["fullname"];
+    $email = $_POST["email"];
+
+
+
+    $query = "UPDATE users SET voornaam=:voornaam, achternaam=:achternaam, email:=email WHERE id=id";
+    $insert = $database->prepare($query);
+    $insert->bindParam(':gebruikersnaam', $username);
+    $insert->bindParam(':voornaam', $fullname);
+    $insert->bindParam(':email', $email);
+    $insert->bindParam(':userId', $id);
+    $insert->execute();
+    header('Location: login.html');
+
+
+}
+
+?>
