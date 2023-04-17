02bb1d2e471ab0f4a75fcd64c55738b6f4d0aebb
Martijn <mhjnieuwenburg@gmail.com>
2023-04-17 12:06:17 +0200

    Messages pop up screen

---
+++ project-TETRIS/messages/messages.js
@@ -0,0 +1,9 @@
+		// Functie om de popup te openen
+		function openMessages() {
+			document.getElementById("Message").style.display = "block";
+		}
+
+		// Functie om de popup te sluiten
+		function closeMessages() {
+			document.getElementById("Message").style.display = "none";
+		}
